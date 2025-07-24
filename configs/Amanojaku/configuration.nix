286e5f60fe3ab222e7bcf7413ba2cd2bd0f79bf9
l00809570 <liyifeng17@hisilicon.com>
2025-07-24 11:27:00 +0800

    temporal

--- configs/Amanojaku/configuration.nix
+++ configs/Amanojaku/configuration.nix
@@ -1,7 +1,21 @@
 { inputs, ... }:
 let 
   inherit (inputs) nixpkgs home-manager nixos-wsl flake-parts import-tree sops-nix;
-in 
+in
 {
-  
+  # register configuration infos into flake.meta
+  flake.meta.machines.Amanojaku = {
+    host_modules = [
+      common
+      wsl
+    ];
+    user_modules = [
+      aor
+    ];
+  };
+
+  # import machine specific configurations
+  imports = [
+    ./specifics
+  ];
 };
