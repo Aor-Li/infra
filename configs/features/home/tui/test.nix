ed2352e6302c26a8a6727eb0779c8b0e141b0961
aor <liyifeng0039@gmail.com>
2025-08-06 02:39:26 +0800

    temporal

--- configs/features/home/tui/test.nix
+++
@@ -1,8 +0,0 @@
-{
-  flake.modules.homeManager."feature/tui" = 
-    { pkgs, ... }:
-    {
-     programs.fd.enable = true;
-     programs.fzf.enable = true;
-    };
-}
---
+++ configs/features/home/tui/yazi.nix
@@ -0,0 +1,6 @@
+{
+  flake.modules.homeManager."feature/tui" = 
+  {
+    programs.yazi.enable = true;
+  };
+}
