70723bd04ef08207a51d9b7f01cf7b892b2df231
l00809570 <liyifeng17@hisilicon.com>
2025-09-02 10:50:02 +0800

    fix home config

--- modules/features/tui/git.nix
+++ modules/features/tui/git.nix
@@ -17,13 +17,6 @@ in
           };
         };
 
-        environments.variables = {
-          # todo: 移动到cargo相关设置
-          CARGO_HTTP_CHECK_REVOKE = "false";
-          CARGO_NET_GIT_FETCH_WITH_CLI = "true";
-          GIT_SSL_NO_VERIFY = "1";
-        };
-
         programs.lazygit.enable = true;
       };
   };
