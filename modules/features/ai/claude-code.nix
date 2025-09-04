6bd7ee2aff6d1e74a60da9eaa64a75f02c49b4c0
l00809570 <liyifeng17@hisilicon.com>
2025-09-04 19:18:01 +0800

    support claude code at work

--- modules/features/ai/claude-code.nix
+++ modules/features/ai/claude-code.nix
@@ -9,9 +9,28 @@ in
       {
         environment.systemPackages = with inputs.nix-ai-tools.packages.${pkgs.system}; [
           claude-code
-          claude-code-router
         ];
       };
-    homeManager.${name} = { ... }: { };
+    homeManager.${name} =
+      { ... }:
+      {
+        home.file.".claude/settings.json".text = ''
+          {
+            "env": {
+              "no_proxy": "localhost,.rnd.huawei.com",
+              "NO_PROXY": "localhost,.rnd.huawei.com",
+              "CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC": "1",
+              "DISABLE_ERROR_REPORTING": "1",
+              "DISABLE_NON_ESSENTIAL_MODEL_CALLS": "1",
+              "DISABLE_TELEMETRY": "1",
+              "ANTHROPIC_AUTH_TOKEN": "sk-1234",
+              "ANTHROPIC_API_KEY": "sk-1234",
+              "ANTHROPIC_BASE_URL": "http://api.anthropic.rnd.huawei.com",
+              "ANTHROPIC_MODEL": "glm-4.5-air",
+              "ANTHROPIC_SMALL_FAST_MODEL": "qwen3-coder-30b-a3b-instruct"
+            }
+          }
+        '';
+      };
   };
 }
--- modules/prototypes/homes/common.nix
+++ modules/prototypes/homes/common.nix
@@ -4,8 +4,8 @@ let
     { options, lib, ... }:
     {
       imports = [
-        config.flake.modules.homeManager."feature/system"
-        config.flake.modules.homeManager."feature/nix"
+        config.flake.modules.homeManager."private/system"
+        config.flake.modules.homeManager."private/nix"
         config.flake.modules.homeManager."feature/network"
       ];
     };
